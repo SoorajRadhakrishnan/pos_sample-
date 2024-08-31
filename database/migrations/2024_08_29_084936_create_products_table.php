765cf5602919e05434e6a871fea3d957106befb3
soorajradhakrishnan <sooriachu@gmail.com>
2024-08-31 10:15:41 +0530

    database collation change

--- database/migrations/2024_08_29_084936_create_products_table.php
+++ database/migrations/2024_08_29_084936_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->decimal('price', 8, 2);
             $table->integer('quantity');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->unsignedBigInteger('category_id');
             $table->timestamps();
 
